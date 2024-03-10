9b25cb0167df0c45941a5a4e6215e379482e4f3c
bababuck <92571492+bababuck@users.noreply.github.com>
2024-03-09 16:18:10 -0800

    Add crossword_compiler.py for interfacing with Cpp generator

---
+++ crossword_compiler.py
@@ -0,0 +1,28 @@
+""" Module for interfacing with the Cpp crossword compiler.
+
+Allows us to invoke the compiler from Python.
+"""
+
+import ctypes
+
+crossword_compiler = ctypes.CDLL(
+    'project-3-crossword-compiler-bababuck/xword.so')
+crossword_compiler.generate.argtypes = (ctypes.c_char_p,
+                                        ctypes.c_char_p,
+                                        ctypes.c_char_p,
+                                        ctypes.c_int)
+# Returns 0 on success, otherwise failure
+crossword_compiler.generate.restype = ctypes.c_int
+
+
+def generate_crossword(wordlist_path,
+                       gridfile_path,
+                       seedlist_path="",
+                       minimum_score=50):
+    wordlist_path = wordlist_path.encode('utf-8')
+    gridfile_path = gridfile_path.encode('utf-8')
+    seedlist_path = seedlist_path.encode('utf-8')
+    return crossword_compiler.generate(wordlist_path,
+                                       gridfile_path,
+                                       seedlist_path,
+                                       minimum_score)

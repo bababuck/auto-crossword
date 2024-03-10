db8ed1516089674348de97cffd117e9c4fdba726
bababuck <92571492+bababuck@users.noreply.github.com>
2024-03-09 16:21:06 -0800

    Update HintGenerator to use AI to interface with GPT

--- hint_generator.py
+++ hint_generator.py
@@ -3,12 +3,27 @@
 Functionality is to ask for a clue for a certain word.
 """
 
+from openai import OpenAI
+
 
 class HintGenerator:
     """Knows how to generate a crossword hint."""
     def __init__(self):
-        pass
+        """Initialize OpenAI client."""
+        self.client = OpenAI()
 
     def __call__(self, solution_word):
-        """Generate a crossword hint given a clue."""
-        return solution_word
+        """Generate a crossword hint given a clue.
+
+        Does this via request to GPT.
+        """
+        response = self.client.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
+            messages=[
+                {"role": "user",
+                 "content":
+                 f"write a cross word clue for "
+                 f"{solution_word} using the fewest tokens possible."}
+            ]
+        )
+        return response.choices[0].message.content
